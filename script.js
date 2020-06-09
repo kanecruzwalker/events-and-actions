5d7df4b873f30369618c645807f34fcdb361a052
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-08 22:16:48 -0700

    added event listeners to buttons with calss of .drum

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("I am connected");
+// select elements with class drum
+var button = document.querySelectorAll(".drum");
+
+// for loop to go through all drum classes and add event listeners to each
+for (var i = 0; i < button.length; i++){
+    var listener = button[i].addEventListener("click", handleClick);
+}
+// the function click carries out.
+function handleClick () {
+    console.log("Handle Click Worked");
+}
